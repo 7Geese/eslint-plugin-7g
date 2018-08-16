bae2982749bbbb1b8602be77a8fdba8e0d5cea80
Maxime Parmentier <parmentier.maxime@gmail.com>
2018-08-16 10:53:21 -0700

    add rule to config file

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 module.exports = {
     rules: {
-        'moment-utc': require('./lib/rules/moment-utc')
+        'moment-utc': require('./lib/rules/moment-utc'),
+        'no-classname-on-common-components': require('./lib/rules/no-classname-on-common-components')
     },
     configs: {
         recommended: {
             rules: {
-                '7g/moment-utc': 1
+                '7g/moment-utc': 1,
+                '7g/no-classname-on-common-components': 1
             }
         }
     }
