b2077e6c24b8cfdae1174b3885305aa7397da991
Maxime Parmentier <parmentier.maxime@gmail.com>
2018-08-16 10:47:53 -0700

    add no classname rule

---
+++ tests/lib/rules/no-classname-on-common-components.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var rule = require("../../../lib/rules/no-classname-on-common-components"),
+    RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+    parserOptions: {
+        ecmaVersion: 6,
+        ecmaFeatures: {
+            jsx: true,
+        },
+    }
+});
+var ruleTester = new RuleTester();
+
+ruleTester.run("moment-utc", rule, {
+    valid: [
+        '<ProfileImage imageUrl="test" />',
+        '<button type="submit" />'
+    ],
+    invalid: [
+        {
+            code: '<ProfileImage className="cool-class-name" />',
+            errors: [{ message: "Avoid className prop on common component ProfileImage"}]
+        }
+    ]
+});
