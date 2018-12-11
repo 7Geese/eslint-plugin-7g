3beefdfab2fc58f002182c5df602c91c5b0d6fa6
himynameisdave <d@velunny.com>
2018-12-11 10:52:43 -0800

    Merges branch 'master' into 2.x

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-'use strict';
+
 
 module.exports = {
     rules: {
-        'moment-utc': require('./lib/rules/moment-utc'),
-        'no-classname-on-common-components': require('./lib/rules/no-classname-on-common-components')
+        'no-classname-on-common-components': require('./rules/no-classname-on-common-components'),
     },
     configs: {
         recommended: {
             rules: {
-                '7g/moment-utc': 1,
-                '7g/no-classname-on-common-components': 1
-            }
-        }
-    }
+                '7g/no-classname-on-common-components': 'warn',
+            },
+        },
+    },
 };
