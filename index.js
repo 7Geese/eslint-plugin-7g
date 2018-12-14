fa3a420503f73826ea7c5c86b135611e14f8cf26
himynameisdave <d@velunny.com>
2018-12-14 09:26:58 -0800

    ⁉️ Whoops, forgot to import the proper rule

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = {
     rules: {
         'no-classname-on-common-components': require('./rules/no-classname-on-common-components.js'),
-        'no-sg-in-components': require('./rules/no-classname-on-common-components.js'),
+        'no-sg-in-components': require('./rules/no-sg-in-components.js'),
     },
     configs: {
         recommended: {
